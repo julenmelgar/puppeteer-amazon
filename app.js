83f0ea0950a07987fafa0b9b98b605161ee52ffd
Julen Melgar Montenegro <julenmelgar@gmail.com>
2022-05-23 08:28:22 +0200

    First commmit

---
+++ app.js
@@ -0,0 +1,74 @@
+// load puppeteer
+const puppeteer = require('puppeteer');
+const domain = "https://www.amazon.es";
+
+// IIFE
+(async () => {
+  // wrapper to catch errors
+  try {
+    // create a new browser instance
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    // create a page inside the browser;
+    const page = await browser.newPage();
+
+    // navigate to a website and set the viewport
+    await page.setViewport({
+      width: 1280,
+      height: 800
+    });
+
+    await page.goto(domain, {
+      waitUntil: 'networkidle2'
+    });
+
+    // accept cookies
+
+    try {
+      await page.waitForSelector('#sp-cc');
+      await page.click('#sp-cc-accept');
+    } catch (error) {
+      console.log('No cookies');
+    }
+
+    // search and wait the product list
+    await page.type('#twotabsearchtextbox', 'iphone x 64gb');
+    await page.click('#nav-search-submit-button');
+    await page.waitForSelector('.s-image');
+
+    // create a screenshots
+    await page.screenshot({
+      path: 'screenshot/search-iphone-x.png'
+    });
+
+    const products = await page.evaluate(() => {
+      const links = Array.from(document.querySelectorAll('.s-result-item.sg-col-4-of-12'));
+
+      return links.map(link => {
+        if (link.querySelector(".a-price-whole")) {
+
+          return {
+            name: link.querySelector("h2").textContent,
+            url: link.querySelector(".a-link-normal.a-text-normal").href,
+            image: link.querySelector(".s-image").src,
+            price: parseFloat(link.querySelector(".a-price-whole").textContent.replace(/[,.]/g, m => (m === ',' ? '.' : ''))),
+          };
+        }
+      }).slice(0, 5);
+    });
+
+    console.log(products.sort((a, b) => {
+      return a.price - b.price;
+    }));
+
+    // close the browser
+    await browser.close();
+
+  } catch (error) {
+    // display errors
+    console.log(error)
+  }
+
+})();
---
+++ index.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+const domain = "https://getbootstrap.com/docs/4.0/examples/checkout/";
+
+(async () => {
+    try {
+
+        // create a new browser instance
+        const browser = await puppeteer.launch({
+            headless: false,
+        });
+
+        // create a page inside the browser;
+        const page = await browser.newPage();
+
+        // navigate to a website and set the viewport
+        await page.setViewport({
+            width: 1280,
+            height: 800
+        });
+
+        await page.goto(domain, {
+            waitUntil: 'networkidle2'
+        });
+
+        await page.waitForSelector('.needs-validation');
+
+        // Input text
+        await page.type('.needs-validation #firstName', 'Julen');
+        // Wait 2 seconds
+        await page.waitForTimeout(2000);
+        // Input select
+        await page.select('.needs-validation #country', 'United States');
+        // Input checkbox
+        await page.click('.needs-validation #same-address');
+        // Input radio
+        await page.click('.needs-validation #paypal');
+        // Wait 2 seconds
+        await page.waitForTimeout(2000);
+        // Button click
+        await page.click('.needs-validation .btn');
+
+        // close the browser
+        await browser.close();
+
+    } catch (error) {
+        console.log(error);
+    }
+})();
